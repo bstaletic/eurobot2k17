7114db4f32c73938daa5e12bbe94b571cb9f7895
Boris Staletic <boris.staletic@gmail.com>
2017-01-19 00:15:50 +0100

    Remove files wrongfully made on this branch

--- src/executors/colour_shifter/colour_shifter.c
+++
@@ -1,6 +0,0 @@
-#include <executors/colour_shifer/colour_shifer.h>
-
-void colour_shift(colour_enum_t colour)
-{
-
-}
--- src/executors/colour_shifter/colour_shifter.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLOUR_SHIFTER
-#define COLOUR_SHIFTER
-
-#include <drivers/sensors/colour/colour.h>
-
-
-void colour_shift(colour_enum_t colour);
-
-#endif /* ifndef COLOUR_SHIFTER */
