9ab036ea8c980837042049db0ff3afe7921f0dc6
Boris Staletic <boris.staletic@gmail.com>
2017-01-18 18:26:56 +0100

    Implement trunk executor

--- src/executors/trunk/trunk.c
+++ src/executors/trunk/trunk.c
@@ -1,11 +1,11 @@
-#include <drivers/actuators/analog_servo/analog_servo.h>
+#include <executors/trunk/trunk.h>
 
 void open_trunk(void)
 {
 	analog_servo servo;
 	servo.channel = OPEN_TRUNK_TIMER_CHANNEL;
 	servo.timer = OPEN_TRUNK_TIMER;
-	servo.value = OPEN_TRUNK_VALUE;
+	analog_servo_move(servo, OPEN_TRUNK_VALUE);
 }
 
 void close_trunk(void)
@@ -13,7 +13,7 @@ void close_trunk(void)
 	analog_servo servo;
 	servo.channel = OPEN_TRUNK_TIMER_CHANNEL;
 	servo.timer = OPEN_TRUNK_TIMER;
-	servo.value = CLOSE_TRUNK_VALUE;
+	analog_servo_move(servo, CLOSE_TRUNK_VALUE);
 }
 
 void increase_trunk(void)
@@ -21,13 +21,13 @@ void increase_trunk(void)
 	analog_servo servo;
 	servo.channel = INCREASE_TRUNK_TIMER_CHANNEL;
 	servo.timer = INCREASE_TRUNK_TIMER;
-	servo.value = INCREASE_TRUNK_VALUE;
+	analog_servo_move(servo, INCREASE_TRUNK_VALUE);
 }
 
-void increase_trunk(void)
+void reduce_trunk(void)
 {
 	analog_servo servo;
 	servo.channel = INCREASE_TRUNK_TIMER_CHANNEL;
 	servo.timer = INCREASE_TRUNK_TIMER;
-	servo.value = REDUCE_TRUNK_VALUE;
+	analog_servo_move(servo, REDUCE_TRUNK_VALUE);
 }
