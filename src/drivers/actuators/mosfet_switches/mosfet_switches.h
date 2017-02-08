e53beaf2f3f6866a86bdeb234064610d33d95501
darko31 <darkoalavanja94@gmail.com>
2017-02-08 20:14:56 +0100

    Mosfet switches config

--- src/drivers/actuators/mosfet_switches/mosfet_switches.h
+++ src/drivers/actuators/mosfet_switches/mosfet_switches.h
@@ -3,8 +3,8 @@
 
 #include <initialisation/gpio_config.h>
 
-void switch_set(uint8_t id);
-void switch_clear(uint8_t id);
-void switch_toggle(uint8_t id);
+void mosfet_set(uint8_t id);
+void mosfet_clear(uint8_t id);
+void mosfet_toggle(uint8_t id);
 
 #endif /* ifndef MOSFET_SWITCH */
