abf1fb9e3471b78c81a5716ea2d2b3f95dc5d708
darko31 <darkoalavanja94@gmail.com>
2017-02-08 20:14:56 +0100

    Mosfet switches config

--- src/drivers/actuators/mosfet_switches/mosfet_switches.c
+++ src/drivers/actuators/mosfet_switches/mosfet_switches.c
@@ -1,14 +1,157 @@
 #include <drivers/actuators/mosfet_switches/mosfet_switches.h>
 
-const analog_servo servo1={TIM9, TIM_OC2};
-const analog_servo servo2={TIM9, TIM_OC1};
-const analog_servo servo4={TIM4, TIM_OC2};
-const analog_servo servo5={TIM3, TIM_OC2};
-const analog_servo servo6={TIM3, TIM_OC1};
-const analog_servo servo7={TIM2, TIM_OC1};
-const analog_servo servo8={TIM1, TIM_OC1};
-
-void analog_servo_move(analog_servo as, int16_t angle)
+void mosfet_set(uint8_t id)
 {
-	timer_set_oc_value(as.timer, as.channel, angle);
+	switch(id){
+
+		case 1:
+
+			gpio_set(GPIOD, GPIO3);
+			break;
+
+		case 2:
+
+			gpio_set(GPIOD, GPIO2);
+			break;
+
+		case 3:
+
+			gpio_set(GPIOD, GPIO1);
+			break;
+
+		case 4:
+
+			gpio_set(GPIOD, GPIO0);
+			break;
+
+		case 5:
+
+			gpio_set(GPIOC, GPIO11);
+			break;
+
+		case 6:
+
+			gpio_set(GPIOC, GPIO9);
+			break;
+
+		case 7:
+
+			gpio_set(GPIOC, GPIO8);
+			break;
+
+		case 8:
+
+			gpio_set(GPIOC, GPIO6);
+			break;
+
+		default:
+			/* Error handling */
+			;
+
+	}
+	
+}
+
+void mosfet_clear(uint8_t id)
+{
+	switch(id){
+
+		case 1:
+
+			gpio_clear(GPIOD, GPIO3);
+			break;
+
+		case 2:
+
+			gpio_clear(GPIOD, GPIO2);
+			break;
+
+		case 3:
+
+			gpio_clear(GPIOD, GPIO1);
+			break;
+
+		case 4:
+
+			gpio_clear(GPIOD, GPIO0);
+			break;
+
+		case 5:
+
+			gpio_clear(GPIOC, GPIO11);
+			break;
+
+		case 6:
+
+			gpio_clear(GPIOC, GPIO9);
+			break;
+
+		case 7:
+
+			gpio_clear(GPIOC, GPIO8);
+			break;
+
+		case 8:
+
+			gpio_clear(GPIOC, GPIO6);
+			break;
+
+		default:
+			/* Error handling */
+			;
+			
+	}
+	
+}
+
+void mosfet_toggle(uint8_t id)
+{
+	switch(id){
+
+		case 1:
+
+			gpio_toggle(GPIOD, GPIO3);
+			break;
+
+		case 2:
+
+			gpio_toggle(GPIOD, GPIO2);
+			break;
+
+		case 3:
+
+			gpio_toggle(GPIOD, GPIO1);
+			break;
+
+		case 4:
+
+			gpio_toggle(GPIOD, GPIO0);
+			break;
+
+		case 5:
+
+			gpio_toggle(GPIOC, GPIO11);
+			break;
+
+		case 6:
+
+			gpio_toggle(GPIOC, GPIO9);
+			break;
+
+		case 7:
+
+			gpio_toggle(GPIOC, GPIO8);
+			break;
+
+		case 8:
+
+			gpio_toggle(GPIOC, GPIO6);
+			break;
+
+		default:
+			/* Error handling */
+			;
+			
+	}
+	
 }
