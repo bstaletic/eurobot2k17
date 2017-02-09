157dd08551d0798b63aea1391140321fad854b80
darko31 <darkoalavanja94@gmail.com>
2017-02-09 21:09:00 +0100

    rocket executor

---
+++ src/executors/rocket/rocket.h
@@ -0,0 +1,15 @@
+#ifndef ROCKET_EXECUTOR
+#define ROCKET_EXECUTOR
+
+#define SERVO_ROCKET servo1
+#define ROCKET_INIT_POSITION 1000
+#define ROCKET_LAUNCH_POSITION 2000
+
+
+#include "../../drivers/actuators/analog_servo/analog_servo.h"
+
+void rocket_init(void);
+
+void rocket_launch(void);
+
+#endif /* ifndef ROCKET_EXECUTOR */
