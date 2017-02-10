547097ed31544b106b837e44acdf606eb7f17820
Boris Staletic <boris.staletic@gmail.com>
2017-02-10 12:19:14 +0100

    Rename for consistency

--- src/executors/trunk/trunk.c
+++ src/executors/trunk/trunk.c
@@ -12,7 +12,7 @@ void trunk_init(void)
 	analog_servo_move(servo, REDUCE_TRUNK_VALUE);
 }
 
-void open_trunk(void)
+void trunk_open(void)
 {
 	analog_servo servo;
 	servo.channel = OPEN_TRUNK_TIMER_CHANNEL;
@@ -20,7 +20,7 @@ void open_trunk(void)
 	analog_servo_move(servo, OPEN_TRUNK_VALUE);
 }
 
-void close_trunk(void)
+void trunk_close(void)
 {
 	analog_servo servo;
 	servo.channel = OPEN_TRUNK_TIMER_CHANNEL;
@@ -28,7 +28,7 @@ void close_trunk(void)
 	analog_servo_move(servo, CLOSE_TRUNK_VALUE);
 }
 
-void increase_trunk(void)
+void trunk_increase(void)
 {
 	analog_servo servo;
 	servo.channel = INCREASE_TRUNK_TIMER_CHANNEL;
@@ -36,7 +36,7 @@ void increase_trunk(void)
 	analog_servo_move(servo, INCREASE_TRUNK_VALUE);
 }
 
-void reduce_trunk(void)
+void trunk_reduce(void)
 {
 	analog_servo servo;
 	servo.channel = INCREASE_TRUNK_TIMER_CHANNEL;
