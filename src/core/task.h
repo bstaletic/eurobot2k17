b3ee8af52212efe518a2112e1e8229eabf78bd2d
Tomislav Tumbas <to92me@gmail.com>
2017-02-10 22:55:38 +0100

    finished first version of task manager

--- src/core/task.h
+++ src/core/task.h
@@ -16,7 +16,8 @@ typedef enum{
 	TASK_USER, ///< user tasks (collect balls task ... )
 	TASK_SYSTEM_PREEXECUTE,  /*!<  system tasks are tasks that have to be updated every cycle ( set glovbal time, update led, ... )
 	 	 	 	 	 	 	 	 Tasks with this type will be executed before user tasks */
-	TASK_SYSTEM_POSTEXECUTE ///< same as TASK_SYSTEM_PREEXECUTE but this tasks will be executed after user task.
+	TASK_SYSTEM_POSTEXECUTE, ///< same as TASK_SYSTEM_PREEXECUTE but this tasks will be executed after user task.
+	TASK_BACKPROC ///< this task will be run while robot is in motion wait state
 } task_types_t;
 
 
@@ -26,7 +27,7 @@ typedef struct {
 	void* world_state; ///< @todo todo edit when world state is completed
 	uint32_t time; ///< time to end of the match;
 	uint32_t priority; ///< priority
-	uint32_t estimaed_time; ///< estimated time of execution
+	uint32_t estimated_time; ///< estimated time of execution
 	task_states_t state; ///< task state, for more @see task_types_t
 
 } task_arguments_t;
@@ -39,13 +40,12 @@ typedef struct {
 
 	task_callback init; ///< pointer to init function. This function is be called at starting of task.
 	task_callback run; ///< pointer to run function This function is called when task is active
-	task_callback resume; ///< pointer to resume function. This function is called when task status went from blocked to active status
 	task_callback finish; ///< pointer to finish function. This function is called when task status went from active to done.
+	task_callback calculate_priority; ///< as name says.
 
 	task_arguments_t* data; ///< @see task_arguments_t
+	char task_name[50]; ///< task name
+
 } task_t;
-
-
-
 
 #endif
