111cd6d121b6e665745084686ac35a4725a675f3
Boris Staletic <boris.staletic@gmail.com>
2017-02-10 12:53:15 +0100

    Initial colour shifter executor - API only

---
+++ src/executors/colour_shifter/colour_shifter.c
@@ -0,0 +1,11 @@
+#include "colour_shifter.h"
+
+void colour_shift(colour_enum colour)
+{
+	while (colour != read_colour())
+	{
+		//rotate DC motor by 5 degrees?
+	}
+
+	//offset needed for positioning?
+}
---
+++ src/executors/colour_shifter/colour_shifter.h
@@ -0,0 +1,9 @@
+#ifndef COLOUR_SHIFTER
+#define COLOUR_SHIFTER
+
+#include "../../drivers/sensors/colour/colour.h"
+#include "../../drivers/actuators/dc_motor/dc_motor.h"
+
+void colour_shift(colour_enum colour);
+
+#endif /* ifndef COLOUR_SHIFTER */
