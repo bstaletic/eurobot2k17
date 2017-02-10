d4148c6ce50857e8bb6e5dde10729cfb3ba6bdbc
Tomislav Tumbas <to92me@gmail.com>
2017-02-10 22:57:18 +0100

    added world state

--- src/core/world_state.h
+++ src/core/world_state.h
@@ -1,6 +1,8 @@
 #ifndef CORE_WORLD_STATE_H
 #define CORE_WORLD_STATE_H
 
+#include "utils/common_utils.h"
+
 typedef enum{
 	ELEMENT_ACTIVE,
 	ELEMENT_IN_ROBOT,
@@ -16,13 +18,13 @@ typedef enum{
 
 typedef enum{
 	ELEMENT_COMMON,
-	ELEMENT_OUR
+	ELEMENT_ALLY
 }element_distribution_t;
 
 typedef struct{
-	int x;
-	int y;
-}point_t;
+	mechanism_status_t status;
+	int elements_count;
+}mechanism_t;
 
 typedef struct{
 	element_status_t status;
@@ -31,14 +33,34 @@ typedef struct{
 	point_t position;
 }element_t;
 
-
 /// @todo todo remove n from ifndef :D
-#ifndef ROBOT_ENTERPRISE
+#ifndef ROBOT_VOYAGER
+typedef struct{
+	mechanism_t revolver;
+	mechanism_t rocket_luncher;
+}robot_state;
 
+typedef struct{
+	element_t rocket_module_ally;
+	element_t rocket_module_common;
+	element_t lunar_module_ally_1;
+	element_t lunar_module_ally_2;
+	element_t lunar_module_common_1;
+	element_t lunar_module_common_2;
+	element_t lunar_module_common_3;
+}world_state;
 
+#else
 
-#else if ROBOT_VOYAGER
+typedef struct{
+	mechanism_t ball_colector;
+};
 
+typedef struct{
+	element_t ball;
+};
+
+typee
 
 #endif
 
