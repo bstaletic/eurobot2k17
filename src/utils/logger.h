7598750db676240b724c0b12c5c25eca59ce3d82
Tomislav Tumbas <to92me@gmail.com>
2017-02-10 22:56:36 +0100

    added logger and common_utils

---
+++ src/utils/logger.h
@@ -0,0 +1,26 @@
+#ifndef UTILS_LOGGER_H
+#define UTILS_LOGGER_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <libopencm3/stm32/usart.h>
+#include <string.h>
+
+
+#define LOGGER_BUFFER_SIZE 512
+
+// todo @todo create struct for formated output
+//typedef struct{
+//	char task_name[50];
+//
+//};
+
+
+void print_header(void);
+
+void error(const char* format, ...);
+void debug(const char* format, ...);
+void info(const char* format, ...);
+
+
+#endif
