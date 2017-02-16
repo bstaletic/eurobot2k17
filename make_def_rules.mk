98f0abfc5b242b85cfbeb2dd1cdcc44e869182a1
Boris Staletic <boris.staletic@gmail.com>
2017-02-16 16:25:58 +0100

    Silence makefile

--- make_def_rules.mk
+++ make_def_rules.mk
@@ -1,6 +1,7 @@
 # Implicit rule for compiling object files and generating dependency rules
 
 $(ASMDIR)%.s: $(SRCDIR)%.c
+	$(VECHO) "Compiling $(subst .c,.o,$<)"
 	@$(MKDIR_P) $(@D) $(subst asm,dep,$(@D))
 	$(CC) $(CFLAGS) $(MFLAGS) -MD -MP -x c -DSTM32F4 -DSTM32F407VG -DSTM32F4CCM -I$(SRCDIR) -Ilibopencm3/include -S $< -o $@
 	@mv $(subst .s,.d,$@) $(subst asm,dep,$(subst .s,.d,$@))
