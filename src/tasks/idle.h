c88187ed81fad09e8e6e8abbfa0da090fb27ce72
Tomislav Tumbas <to92me@gmail.com>
2017-04-01 13:05:06 +0200

    added idle task

---
+++ src/tasks/idle.h
@@ -0,0 +1,10 @@
+#ifndef TASKS_IDLE_H
+#define TASKS_IDLE_H
+
+#include "../core/task_mngr.h"
+#include "../utils/logger.h"
+#include "cmsis_os.h"
+
+void ctor_task_idle(void);
+
+#endif // TASKS_IDLE_H
