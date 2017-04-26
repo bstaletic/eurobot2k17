0c9633e42c9fae8a743489bd72d43a2b4590b0cd
Tumbas Tomislav <to92me@gmail.com>
2017-04-26 17:41:40 +0200

    fixed startup task and added it in Makefile

--- src/tasks/start_task.c
+++ src/tasks/start_task.c
@@ -1,26 +1,30 @@
 #include "start_task.h"
 
-task_t task;
-task_arguments_t arguments;
+static task_t task;
+static task_arguments_t arguments;
 
 static void start_match(task_arguments_t* argv)
 {
 	// Wait for jumper to be unplugged
-	while(jumper);
+	info("Waiting jumper");
+//	while(jumper);
 	//start TIM11
 	HAL_TIM_Base_Start(&htim11);
 	HAL_TIM_Base_Start_IT(&htim11);
+
+	argv->state = TASK_DONE;
 }
 
 void ctor_start_task(void)
 {
+	debug("CTOR START TASK");
 	task.run = (task_callback)&start_match;
-	task.type = TASK_SYSTEM_PREEXECUTE;
+	task.type = TASK_USER;
 	strncpy(task.task_name, "Start match", 12);
 
 	arguments.time = 0;
 
 	task.data = &arguments;
 
-	set_start_task(task);
+	set_start_task(&task);
 }
